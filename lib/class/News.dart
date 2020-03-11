b93a832660dabce4896a863509e4cec866000e75
Mattstacey <dhpro1998@gmail.com>
2020-03-11 09:12:25 +0700

    fix error home

--- lib/class/News.dart
+++ lib/class/News.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class News {
   String url;
   String imageUrl;
@@ -26,32 +25,3 @@ class ListNews {
     return ListNews(listNews: _listNews);
   }
 }
-=======
-class News {
-  String url;
-  String imageUrl;
-  String shortText;
-  String time;
-  News({this.url, this.shortText, this.imageUrl, this.time});
-}
-
-class ListNews {
-  List<News> listNews;
-  ListNews({this.listNews});
-  factory ListNews.fromJson(List<dynamic> json) {
-    List<News> _listNews = [];
-    try {
-      json.forEach((e) {
-        _listNews.add(News(
-            shortText: e['article_short'],
-            imageUrl: e['article_image'],
-            url: e['article_url'],
-            time: DateTime.now().difference(DateTime.parse(e['release_date'])).inDays.toString()));
-      });
-    } catch (e) {
-      return ListNews(listNews: []);
-    }
-    return ListNews(listNews: _listNews);
-  }
-}
->>>>>>> 18325bbeee3512d61d0afa5b5d870b5a902264ca
--- lib/view/profile/row_account_setting.dart
+++ lib/view/profile/row_account_setting.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 final settingFont = TextStyle(fontWeight: FontWeight.bold);
@@ -23,30 +22,4 @@ rowAccountSetting(Icon icon, String text) {
         ),
       ],
     );
-=======
-import 'package:flutter/material.dart';
-
-final settingFont = TextStyle(fontWeight: FontWeight.bold);
-rowAccountSetting(Icon icon, String text) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(left: 30),
-          child: Row(
-            children: <Widget>[
-              icon,
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                text,
-                style: settingFont,
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
->>>>>>> 18325bbeee3512d61d0afa5b5d870b5a902264ca
   }
